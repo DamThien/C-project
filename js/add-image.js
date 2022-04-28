cfa562460588dde6d294a82afec0c5080ded5a22
unknown <admin@PNVD203.pn.com.vn>
2022-04-28 13:44:08 +0700

    updatefinish

--- js/add-image.js
+++ js/add-image.js
@@ -12,7 +12,7 @@ previewBeforeUpload("file");
 
 var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close-icon")[0];
 btn.onclick = function() {
     modal.style.display = "block";
 
