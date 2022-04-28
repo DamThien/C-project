bf44b19a7d26b697bd5446dce77d88a4a099f860
unknown <admin@PNVD203.pn.com.vn>
2022-04-28 11:40:44 +0700

    nguyet update

---
+++ js/add-image.js
@@ -0,0 +1,27 @@
+function previewBeforeUpload(id) {
+    document.querySelector("#" + id).addEventListener("change", function(e) {
+        if (e.target.files.length == 0) {
+            return;
+        }
+        let file = e.target.files[0];
+        let url = URL.createObjectURL(file);
+        document.querySelector("#" + id + "-preview img").src = url;
+    });
+}
+previewBeforeUpload("file");
+
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
